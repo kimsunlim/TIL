3d5a4d6edca69c38cbdfd6583eefa27177b73a4c
kimsunlim <71312462+kimsunlim@users.noreply.github.com>
2022-01-12 16:27:46 +0900

    Update 152.Sorting_sunlim.py

--- Algorithm_Python/Baekjoon/algorithm_study/152.Sorting_sunlim.py
+++ Algorithm_Python/Baekjoon/algorithm_study/152.Sorting_sunlim.py
@@ -15,7 +15,9 @@ for _ in range (N):
     user.append(list(input().split())) #나이와 이름 공백으로 구분
 
 #나이를 기준으로 정렬
-user.sort(key=lambda a:int(a[0])) #각 행의 첫번째 인덱스를 기준으로, 즉 나이를 기준으로 정렬!
+user.sort(key=lambda a:int(a[0])) #각 행의 첫번째 인덱스를 기준으로, 즉 나이를 기준으로 정렬! 
+                                  # 정렬을 목적으로 하는 함수를 값으로 넣는다. lambda이용
+                                  #key에 함수를 넘겨주면 해당 함수의 반환값을 비교하여 순서대로 정렬함!
 
 #출력
 for i in range(N):

aeca4c04c0f9cc8ba0f1b223dcc5f448e3f888b1
kimsunlim <71312462+kimsunlim@users.noreply.github.com>
2022-05-23 15:33:24 +0900

    22.05.23 | add integer

---
+++ Algorithm_Python/Baekjoon/step/15. 정수론및조합론/Boj.5086.py
@@ -0,0 +1,19 @@
+### 정수론 및 조합론
+
+#백준 5086 배수와 약수
+
+
+
+while True:
+    n,m =map(int,input().split())
+    if n==0 and m==0:
+        break
+    if m % n ==0: # 첫번째 숫자가 두번째 숫자의 약수일경우
+        print('factor')
+        
+    elif n % m ==0: #첫번째숫자가 두번째 숫자의 배수일 경우
+        
+        print('multiple')
+        
+    else: 
+        ('neither')

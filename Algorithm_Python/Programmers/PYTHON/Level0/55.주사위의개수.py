74d0e6ed4bd37a9e16e4c4a6da8808e6a3c63c88
kimsunlim <71312462+kimsunlim@users.noreply.github.com>
2022-11-18 21:44:23 +0900

    22.11.18 | add index

---
+++ Algorithm_Python/Programmers/PYTHON/Level0/55.주사위의개수.py
@@ -0,0 +1,13 @@
+
+#https://school.programmers.co.kr/learn/courses/30/lessons/120845
+#주사위의 개수
+#인덱스 활용하기 
+
+
+
+def solution(box,n):
+    a=box[0]//n
+    b=box[0]//n
+    c=box[0]//n
+
+    return a*b*c
